2ebb160b5c5b1e34a62b585d861a2ee20cc4bfa2
bptechgit <birendra.itpro@gmail.com>
2024-06-22 12:29:44 +0000

    Conditional Statement

---
+++ condition.py
@@ -0,0 +1,12 @@
+num1 = input("Please Enter First Number:") 
+num2 = input("Please Enter Second Number:")
+
+if int(num1) > int(num2):
+    print("Number1 is Greater than Number2")
+
+elif int(num1) < int(num2):
+    print("Number1 is Less than Number2")
+
+else:
+    print("Number1 is Equal to Number2")
+
